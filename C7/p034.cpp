4957caed389de80371aad1abe0cbdeb8f6c15e05
MzSamyCpp <motaz@motaz.local>
2025-03-14 23:31:05 +0400

    Update

---
+++ C7/p034.cpp
@@ -0,0 +1,40 @@
+// vid 21
+// program that prints the fibonacci sequence with recursion
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+
+void Fibonacci(int Number);
+
+int main()
+{
+    Fibonacci(10);
+}
+// 1 1 2 3 5 8 13
+void Fibonacci(int Number)
+{
+    int Fibonacci = 0;
+    int Prev2 = 0;
+    int Prev1 = 1;
+
+    std::cout << "1 ";
+    for (int i = 2; i <= Number; i++)
+    {
+        Fibonacci = Prev2 + Prev1;
+        if (i == Number)
+            std::cout << Fibonacci;
+        else
+            std::cout << Fibonacci << " ";
+        Prev2 = Prev1;
+        Prev1 = Fibonacci;
+    }
+}
+void RecFibonacci(int Number)
+{
+    int Fibnacci = 0;
+    int Prev1 = 0;
+    int Prev2 = 1;
+    std::cout << 
+}

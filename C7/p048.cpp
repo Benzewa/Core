1d47005d7aee0311b8f02aec82359dd679cf00a0
Benzewa <mzsamy22@gmail.com>
2025-03-21 22:18:55 +0400

    Up

--- C7/p048.cpp
+++ C7/p048.cpp
@@ -1,2 +1,32 @@
-// vid 27
-// program that Reads a string then uppercases and lowercases all Letters
+// vid 28
+// program that Reads a string and converts all characters small convert to capital
+// if its capital convert to small
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cctype>
+
+void ReadString(std::string &Str);
+void InvertStr(std::string &Str);
+
+int main()
+{
+    std::string String;
+    ReadString(String);
+    std::cout << String << "\n";
+    InvertStr(String);
+    std::cout << String << "\n";
+}
+void ReadString(std::string &Str)
+{
+    std::cout << "Enter a String\n";
+    getline(std::cin, Str);
+}
+void InvertStr(std::string &Str)
+{
+    for (int i = 0; i < Str.length(); i++)
+        isupper(Str[i]) ? tolower(Str[i]) : toupper(Str[i]);
+}
